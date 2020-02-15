b04d9ee309f77ca7db859773d28bb0590b4bf96b
Alex RC <alexis.rcas@gmail.com>
2020-02-15 15:42:23 +0000

    barras animadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,48 @@
 jQuery(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
+    $('.cap').css('opacity', 0.7);
 
+    let growBars = () => {
         $('.level-bar-inner').each(function() {
         
             var itemWidth = $(this).data('level');
             var opacity = parseInt(itemWidth)
     
-            $(this).css('opacity', itemWidth)
+            $(this).css('opacity', 0.7)
+
+            if (parseInt($(this).data('level')) < 40) {
+                $(this).css('background', '#D85858')
+            }
+            else if(parseInt($(this).data('level')) < 75) {
+                $(this).css('background', '#D8D061')
+            }
+            else {
+                $(this).css('background', '#49AE61')
+            }
             
             $(this).animate({
                 width: itemWidth
-            }, 800);
+            }, 1600);
             
         });
+    }
 
+
+    /*$(window).on('load', function() {
+        growBars();
+    });*/
+
+    $(window).scroll(function() {
+        var top_of_element = $('#start-grow').offset().top;
+        var bottom_of_element = $('#start-grow').offset().top + $('#start-grow').outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+    
+        if ((bottom_of_screen > top_of_element + 200) && (top_of_screen < bottom_of_element)){
+            growBars();
+        }
     });
    
-    
 
 });
